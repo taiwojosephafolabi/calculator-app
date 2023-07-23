e9048930e4ce3949afcfc8d309a56a08f72dc2bd
JTAfolabi <taiwojosephafolabi@gmail.com>
2023-07-23 23:13:11 +0100

    calculator component

--- frontend/src/components/Calculator.js
+++ frontend/src/components/Calculator.js
@@ -1,11 +1,18 @@
 import React from 'react';
 
-function Calculator({}){
+function Calculator({calculatorApp}) {
   return (
-    <div>
-
+    <div className='calculator-grid'>
+      <div className='output'>
+        <div className='previous-operand'></div>
+        <div className='current-operand'></div>
+      </div>
+      <button className='span-two'>AC</button>
+      <button>DEL</button>
+      {/* Input numbers and operands */}
+      <button></button>
     </div>
-  )
+  );
 }
 
 export default Calculator;
